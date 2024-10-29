2a63378e9b46a34d953cc3a4b6a8fb6770e3a1af
AbetiouSN <sofianeabetiou@gmail.com>
2024-10-29 20:31:58 +0100

    add API-GATEWAY

--- API-GATEWAY/src/main/java/com/abetiou/apigateway/ApiGatewayApplication.java
+++ API-GATEWAY/src/main/java/com/abetiou/apigateway/ApiGatewayApplication.java
@@ -1,4 +1,4 @@
-package com.springsecurity.springsecurity;
+package com.abetiou.apigateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -6,10 +6,10 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
 @EnableDiscoveryClient
-public class SpringsecurityApplication {
+public class ApiGatewayApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SpringsecurityApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ApiGatewayApplication.class, args);
+    }
 
 }
